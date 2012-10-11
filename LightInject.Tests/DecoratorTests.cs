5cbdd7cf449e08555e7b9abe35838408ff89d6cf
Bernhard Richter <bernhard.richter@gmail.com>
2012-10-11 15:07:57 +0200

    More decorator tests

--- LightInject.Tests/DecoratorTests.cs
+++ LightInject.Tests/DecoratorTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace LightInject.Tests
 {
+    using System.Linq;
+
     using LightInject.SampleLibrary;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -29,6 +31,19 @@
         }
 
         [TestMethod]
+        public void GetAllInstances_WithDecorator_ReturnsDecoratedInstances()
+        {
+            var container = CreateContainer();
+            container.Register<IFoo, Foo>();
+            container.Register<IFoo, AnotherFoo>("AnotherFoo");
+            container.Decorate(typeof(IFoo), typeof(FooDecorator), si => true);
+            var instances = container.GetAllInstances<IFoo>();
+            Assert.IsInstanceOfType(instances.First(), typeof(FooDecorator));
+            Assert.IsInstanceOfType(instances.Last(), typeof(FooDecorator));
+        }
+
+
+        [TestMethod]
         public void GetInstance_WithOpenGenericDecorator_ReturnsDecoratedInstance()
         {
             var container = CreateContainer();
@@ -48,6 +63,31 @@
             var instance = container.GetInstance<IFoo<int>>();
             Assert.IsInstanceOfType(instance, typeof(AnotherFooDecorator<int>));
         }
+
+        [TestMethod]
+        public void GetInstance_ClosedGenericServiceWithOpenGenericDecorator_ReturnsDecoratedInstance()
+        {
+            var container = CreateContainer();
+            container.Register(typeof(IFoo<int>), typeof(Foo<int>));
+            container.Decorate(typeof(IFoo<>), typeof(FooDecorator<>), si => true);
+            var instance = container.GetInstance<IFoo<int>>();
+            Assert.IsInstanceOfType(instance, typeof(FooDecorator<int>));
+        }
+
+        [TestMethod]
+        public void GetAllInstances_WithOpenGenericDecorator_ReturnsDecoratedInstances()
+        {
+            var container = CreateContainer();
+            container.Register(typeof(IFoo<>), typeof(Foo<>));
+            container.Register(typeof(IFoo<>), typeof(AnotherFoo<>), "AnotherFoo");
+            container.Decorate(typeof(IFoo<>), typeof(FooDecorator<>), si => true);
+            var instances = container.GetAllInstances<IFoo<int>>();
+            Assert.IsInstanceOfType(instances.First(), typeof(FooDecorator<int>));
+            Assert.IsInstanceOfType(instances.Last(), typeof(FooDecorator<int>));
+        }
+        
+
+
 
         private static IServiceContainer CreateContainer()
         {
