30b56b3c47468cd39eacb4c8ac32fbc0d7af4101
Bernhard Richter <bernhard.richter@gmail.com>
2013-03-14 17:20:10 +0100

    Updates docs and version number

--- LightInject.Mocking/Properties/AssemblyInfo.cs
+++ LightInject.Mocking/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DIPS ASA")]
 [assembly: AssemblyProduct("LightInject.Mocking")]
-[assembly: AssemblyCopyright("Copyright © DIPS ASA 2013")]
+[assembly: AssemblyCopyright("Copyright © Bernhard Richter 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: InternalsVisibleTo("LightInject.Tests")]
