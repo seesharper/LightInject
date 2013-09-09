137b5837bea108843acef6d4e43c63ce1e4140cd
Bernhard Richter <bernhard.richter@gmail.com>
2013-09-09 23:56:05 +0200

    Added methodbuilder verification

---
+++ LightInject.Interception.Tests/DynamicMethodBuilderVerificationTests.cs
@@ -0,0 +1,17 @@
+﻿namespace LightInject.Interception.Tests
+{
+    using System;
+    using System.IO;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DynamicMethodBuilderVerificationTests : DynamicMethodBuilderTests
+    {
+        protected override IMethodBuilder GetMethodBuilder()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DynamicMethodAssembly.dll");
+            return new DynamicMethodBuilder(() => new MethodBuilderMethodSkeleton(path));
+        }
+    }
+}
---
+++ LightInject.Interception.Tests/MethodBuilderDynamicMethodSkeleton.cs
@@ -0,0 +1,93 @@
+﻿namespace LightInject.Interception.Tests
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Reflection.Emit;
+
+    
+
+    
+    public class MethodBuilderMethodSkeleton : IMethodSkeleton
+    {
+        private readonly string outputPath;
+
+        private readonly string fileName;
+
+        private AssemblyBuilder assemblyBuilder;
+
+        private TypeBuilder typeBuilder;
+
+        private MethodInfo dynamicMethod;
+
+        private MethodBuilder methodBuilder;
+
+        private ILGenerator ilGenerator;
+
+
+        public MethodBuilderMethodSkeleton(string outputPath)
+        {
+            this.outputPath = outputPath;
+            fileName = Path.GetFileName(outputPath);
+            CreateAssemblyBuilder();
+            CreateTypeBuilder();
+            CreateMethodBuilder();
+        }
+
+        private void CreateMethodBuilder()
+        {
+            methodBuilder = typeBuilder.DefineMethod(
+                "DynamicMethod",
+                MethodAttributes.Public | MethodAttributes.Static,
+                typeof(object),
+                new[] { typeof(object), typeof(object[]) });
+            methodBuilder.InitLocals = true;
+            ilGenerator = methodBuilder.GetILGenerator();
+        }
+
+        private void CreateAssemblyBuilder()
+        {
+            AppDomain myDomain = AppDomain.CurrentDomain;
+            assemblyBuilder = myDomain.DefineDynamicAssembly(
+                CreateAssemblyName(), AssemblyBuilderAccess.Save, Path.GetDirectoryName(outputPath));
+        }
+
+        private void CreateTypeBuilder()
+        {
+            var moduleName = Path.GetFileNameWithoutExtension(outputPath);
+            ModuleBuilder module = assemblyBuilder.DefineDynamicModule(moduleName, fileName);
+            typeBuilder = module.DefineType("DynamicType", TypeAttributes.Public);
+        }
+
+        private AssemblyName CreateAssemblyName()
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(outputPath);
+            return new AssemblyName(fileNameWithoutExtension);
+        }
+
+        public void Verify()
+        {
+            Type type = typeBuilder.CreateType();
+            dynamicMethod = type.GetMethod("DynamicMethod");
+            assemblyBuilder.Save(fileName);
+            Console.WriteLine("Saving file " + fileName);
+            AssemblyAssert.IsValidAssembly(outputPath);
+        }
+
+        public ILGenerator GetILGenerator()
+        {
+            return ilGenerator;
+        }
+
+        public Func<object, object[], object> CreateDelegate()
+        {
+            Type type = typeBuilder.CreateType();
+            dynamicMethod = type.GetMethod("DynamicMethod");
+            assemblyBuilder.Save(fileName);
+            Console.WriteLine("Saving file " + fileName);
+            AssemblyAssert.IsValidAssembly(outputPath);
+            var del = Delegate.CreateDelegate(typeof(Func<object, object[], object>), dynamicMethod);
+            return (Func<object, object[], object>)del;
+        }
+    }
+}
