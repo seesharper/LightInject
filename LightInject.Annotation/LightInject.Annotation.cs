6c883b1460ba109b4f395e775b97921bf3076ded
Bernhard Richter <bernhard.richter@gmail.com>
2013-10-30 18:11:28 +0100

    XML doc LightInject.Annotation

--- LightInject.Annotation/LightInject.Annotation.cs
+++ LightInject.Annotation/LightInject.Annotation.cs
@@ -140,8 +140,18 @@ namespace LightInject.Annotation
         }
     }
 
+    /// <summary>
+    /// A <see cref="ConstructorDependencySelector"/> that looks for the <see cref="InjectAttribute"/> 
+    /// to determine the name of service to be injected.
+    /// </summary>
     internal class AnnotatedConstructorDependencySelector : ConstructorDependencySelector
     {
+        /// <summary>
+        /// Selects the constructor dependencies for the given <paramref name="constructor"/>.
+        /// </summary>
+        /// <param name="constructor">The <see cref="ConstructionInfo"/> for which to select the constructor dependencies.</param>
+        /// <returns>A list of <see cref="ConstructorDependency"/> instances that represents the constructor
+        /// dependencies for the given <paramref name="constructor"/>.</returns>
         public override IEnumerable<ConstructorDependency> Execute(ConstructorInfo constructor)
         {
             var constructorDependencies = base.Execute(constructor).ToArray();
