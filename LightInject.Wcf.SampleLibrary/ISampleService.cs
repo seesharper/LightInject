26eb5d3ff4046b2b95e3eb1decb5a8cc0f48c365
Bernhard Richter <bernhard.richter@gmail.com>
2013-11-10 23:35:45 +0100

    Wcf - Missing Files

---
+++ LightInject.Wcf.SampleLibrary/ISampleService.cs
@@ -0,0 +1,25 @@
+﻿namespace LightInject.Wcf.SampleServices
+{
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        int Add(int value1, int value2);
+    }
+
+    [ServiceContract]
+    public interface IServiceWithServiceContractAttribute
+    {
+    }
+
+    public interface IServiceWithoutServiceContractAttribute
+    {
+    }
+
+    [ServiceContract]
+    public class Service
+    {
+    }
+}
