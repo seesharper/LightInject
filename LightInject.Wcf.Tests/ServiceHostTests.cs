86a3c2e12b6e67d72f17aac29388df5609066bce
Bernhard Richter <bernhard.richter@gmail.com>
2013-11-11 23:40:15 +0100

    WCF added VirtualPathProvider

--- LightInject.Wcf.Tests/ServiceHostTests.cs
+++ LightInject.Wcf.Tests/ServiceHostTests.cs
@@ -13,9 +13,8 @@
         [TestMethod]
         public void CreateServiceHost_ServiceTypeIsInterfaceWithServiceContractAttribute_ReturnsServieHostWithProxyAsServiceType()
         {
-            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();
-            var serviceType = typeof(IServiceWithServiceContractAttribute);
-            var serviceHost = lightInjectServiceHostFactory.CreateServiceHost(serviceType.AssemblyQualifiedName, new Uri[] { });
+            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();            
+            var serviceHost = lightInjectServiceHostFactory.CreateServiceHost<IServiceWithServiceContractAttribute>();
             var returnedServiceType = serviceHost.Description.ServiceType;
             Assert.IsTrue(typeof(IProxy).IsAssignableFrom(returnedServiceType));
         }
@@ -23,24 +22,15 @@
         [TestMethod]
         public void CreateServiceHost_ServiceTypeIsConcrete_ThrowsNotSupportedException()
         {
-            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();
-            var serviceType = typeof(ServiceWithoutInterface);
-            ExceptionAssert.Throws<NotSupportedException>(() => lightInjectServiceHostFactory.CreateServiceHost(serviceType.AssemblyQualifiedName, new Uri[] { }));
+            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();            
+            ExceptionAssert.Throws<NotSupportedException>(() => lightInjectServiceHostFactory.CreateServiceHost<ServiceWithoutInterface>());
         }
 
         [TestMethod]
         public void CreateServiceHost_ServiceTypeIsInterfaceWithoutServiceContractAttribute_ThrowsNotSupportedException()
         {
-            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();
-            var serviceType = typeof(IServiceWithoutServiceContractAttribute);
-            ExceptionAssert.Throws<NotSupportedException>(() => lightInjectServiceHostFactory.CreateServiceHost(serviceType.AssemblyQualifiedName, new Uri[] { }));
-        }
-
-        [TestMethod]
-        public void CreateServiceHost_ServiceTypeIsNotKnown_ThrowsArgumentException()
-        {
-            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();
-            ExceptionAssert.Throws<ArgumentException>(() => lightInjectServiceHostFactory.CreateServiceHost("unknown", new Uri[] { }));
-        }      
+            var lightInjectServiceHostFactory = new LightInjectServiceHostFactory();            
+            ExceptionAssert.Throws<NotSupportedException>(() => lightInjectServiceHostFactory.CreateServiceHost<IServiceWithoutServiceContractAttribute>());
+        }     
     }       
 }
