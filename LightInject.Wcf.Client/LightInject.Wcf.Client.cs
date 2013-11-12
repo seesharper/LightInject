ca5deb073fa33dc8d6cceb47a2f0ccd6a35073ed
Bernhard Richter <bernhard.richter@gmail.com>
2013-11-12 20:47:42 +0100

    Wcf.Client Initial commit

---
+++ LightInject.Wcf.Client/LightInject.Wcf.Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightInject.Wcf.Client
+{
+    using System.ServiceModel;
+
+    internal static class WcfContainerExtensions
+    {
+        internal static void EnableWcf(this IServiceContainer serviceContainer)
+        {
+            serviceContainer.RegisterFallback(
+                (serviceType, serviceName) => serviceType.IsDefined(typeof(ServiceContractAttribute), true),
+                CreateWcfProxy);
+        }
+
+        private static object CreateWcfProxy(ServiceRequest serviceRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
