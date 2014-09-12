c44261dbb76b4acaa945f72cc1f93324abb666ea
Bernhard Richter <bernhard.richter@gmail.com>
2014-09-12 14:50:29 +0200

    xUnit Initial commit

---
+++ LightInject.Tests/XunitTests.cs
@@ -0,0 +1,95 @@
+﻿namespace LightInject.Tests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using LightInject.SampleLibrary;
+    using LightInject.Xunit;
+
+    using global::Xunit;
+    using global::Xunit.Extensions;
+
+    public class XunitTestsWithConfigureMethod
+    {
+        [Theory, InjectData]    
+        public void MethodWithOneArgument(IFoo foo)
+        {
+            Assert.NotNull(foo);
+        }
+
+        [Theory, InjectData]
+        public void MethodWithTwoArguments(IFoo foo, IBar bar)
+        {
+            Assert.NotNull(foo);
+            Assert.NotNull(bar);
+        }
+
+        
+        public void MethodWithMissingService(int value)
+        {
+            
+        }
+
+        [Fact]
+        public void MissingServiceThrowsException()
+        {
+            var attribute = new InjectDataAttribute();
+            var method = typeof(XunitTestsWithConfigureMethod).GetMethod("MethodWithMissingService");
+            Assert.Throws<InvalidOperationException>(() => attribute.GetData(method, new Type[] { typeof(int) }));
+        }
+
+        
+
+
+
+
+        internal static void Configure(IServiceContainer container)
+        {
+            Console.WriteLine("Configure" + Thread.CurrentThread.ManagedThreadId);
+            container.Register<IFoo, Foo>();
+            container.Register<IBar, Bar>();
+        }
+    }
+
+
+    public class XunitBaseClass
+    {
+        internal static void Configure(IServiceContainer container)
+        {
+            container.Register<IFoo, Foo>();
+            container.Register<IBar, Bar>();
+        }
+    }
+
+    public class XunitTestsWithConfigureMethodInBaseClass : XunitBaseClass
+    {
+        [Theory, InjectData]
+        public async void MethodWithOneArgument(IFoo foo)
+        {
+            await Task.Delay(100);
+            Assert.NotNull(foo);
+        }
+    }
+
+    public class XUnitTestsWithScopedServices
+    {
+        [ScopedTheory, InjectData]
+        public void MethodWithScopedArgument(IFoo foo)
+        {
+            Assert.NotNull(foo);
+        }
+
+        [ScopedTheory, InjectData]
+        public void MethodWithTwoScopedArguments(IFoo foo, IBar bar)
+        {
+            Assert.NotNull(foo);
+        }        
+
+        internal static void Configure(IServiceContainer container)
+        {
+            container.Register<IFoo, DisposableFoo>(new PerScopeLifetime());
+            container.Register<IBar, Bar>(new PerScopeLifetime());         
+        }
+    }
+}
---
+++ LightInject.Xunit/LightInject.Xunit.cs
@@ -0,0 +1,198 @@
+﻿/*********************************************************************************   
+    The MIT License (MIT)
+
+    Copyright (c) 2014 bernhard.richter@gmail.com
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+******************************************************************************
+    LightInject.xUnit version 1.0.0.2
+    http://www.lightinject.net/
+    http://twitter.com/bernhardrichter
+******************************************************************************/
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1126:PrefixCallsCorrectly", Justification = "Reviewed")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:PrefixLocalCallsWithThis", Justification = "No inheritance")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Single source file deployment.")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:FileMustHaveHeader", Justification = "Custom header.")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "All public members are documented.")]
+
+namespace LightInject.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Remoting.Messaging;
+
+    using LightInject;
+    using global::Xunit.Extensions;
+    using global::Xunit.Sdk;
+
+    /// <summary>
+    /// Allows LightInject to resolve test method arguments.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    internal class InjectDataAttribute : DataAttribute
+    {        
+        private const string Key = "XunitServiceContainer";
+      
+        /// <summary>
+        /// Gets a list of argument values resolved using an <see cref="IServiceContainer"/> instance.
+        /// </summary>
+        /// <param name="methodUnderTest">The test method currently being executed.</param>
+        /// <param name="parameterTypes">The parameter types of the <paramref name="methodUnderTest"/>.</param>
+        /// <returns>A list of argument values resolved using an <see cref="IServiceContainer"/> instance.</returns>
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+        {
+            var container = GetContainer(methodUnderTest.DeclaringType);
+            if (ShouldStartScope(methodUnderTest))
+            {
+                container.BeginScope();    
+            }
+            
+            try
+            {
+                return ResolveParameters(container, parameterTypes);
+            }
+            catch (InvalidOperationException ex)
+            {
+                const string ErrorMessage = "Unable to inject test method arguments. "
+                + "Create a static method in the test class with the following signature "
+                + "to configure the container: public static void Configure(IServiceContainer container)";
+                throw new InvalidOperationException(ErrorMessage, ex);
+            }
+        }
+
+        /// <summary>
+        /// Releases the current <see cref="Scope"/>.
+        /// </summary>
+        /// <param name="type">The test class <see cref="Type"/> for which to release the current <see cref="Scope"/>.</param>
+        internal static void Release(Type type)
+        {
+            var key = CreateContainerKey(type);
+            var containerWrapper = (ContainerWrapper)CallContext.LogicalGetData(key);
+                        
+            if (containerWrapper != null)
+            {
+                containerWrapper.Value.EndCurrentScope();
+            }
+        }
+
+        private static string CreateContainerKey(Type type)
+        {
+            return Key + type.AssemblyQualifiedName;
+        }
+
+        private static bool ShouldStartScope(MethodInfo methodUnderTest)
+        {
+            return methodUnderTest.IsDefined(typeof(ScopedTheoryAttribute), true);
+        }
+
+        private static IServiceContainer GetContainer(Type type)
+        {
+            var containerWrapper = (ContainerWrapper)CallContext.LogicalGetData(Key);
+            if (containerWrapper == null)
+            {
+                containerWrapper = new ContainerWrapper { Value = new ServiceContainer() };
+                InvokeConfigureMethodIfPresent(type, containerWrapper.Value);
+                CallContext.LogicalSetData(Key, containerWrapper);                
+            }
+
+            return containerWrapper.Value;
+        }
+
+        private static IEnumerable<object[]> ResolveParameters(IServiceFactory factory, IEnumerable<Type> parameterTypes)
+        {
+            return new[] { parameterTypes.Select(factory.GetInstance).ToArray() };            
+        }
+
+        private static void InvokeConfigureMethodIfPresent(Type type, IServiceContainer container)
+        {
+            var composeMethod = type.GetMethod(
+                "Configure",
+                BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+            if (composeMethod != null)
+            {
+                composeMethod.Invoke(null, new object[] { container });
+            }
+        }
+
+        [Serializable]
+        private class ContainerWrapper : MarshalByRefObject
+        {
+            [NonSerialized]
+            private IServiceContainer value;
+
+            public IServiceContainer Value
+            {
+                get
+                {
+                    return value;
+                }
+
+                set
+                {
+                    this.value = value;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// A theory that ensures that services registered with the <see cref="PerScopeLifetime"/> or 
+    /// <see cref="PerRequestLifeTime"/> are properly disposed when the test ends.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    internal class ScopedTheoryAttribute : TheoryAttribute
+    {        
+        protected override IEnumerable<ITestCommand> EnumerateTestCommands(IMethodInfo method)
+        {
+            var commands = base.EnumerateTestCommands(method);
+            foreach (var testCommand in commands)
+            {
+                var theoryCommand = testCommand as TheoryCommand;
+                if (theoryCommand != null)
+                {
+                    yield return new ReleaseCommand(theoryCommand, method);
+                }
+                else
+                {
+                    yield return testCommand;
+                }
+            }           
+        }
+                
+        private class ReleaseCommand : TestCommand
+        {
+            private readonly TheoryCommand theory;
+
+            public ReleaseCommand(TheoryCommand theory, IMethodInfo method)
+                : base(method, MethodUtility.GetDisplayName(method), MethodUtility.GetTimeoutParameter(method))
+            {
+                this.theory = theory;                    
+            }
+
+            public override MethodResult Execute(object testClass)
+            {
+                var result = theory.Execute(testClass);                
+                InjectDataAttribute.Release(testClass.GetType());            
+                return result;                
+            }
+        }
+    }    
+}
