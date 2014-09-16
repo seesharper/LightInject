c3f88b9c121c470057d7e062b8f51ebaf6a8849e
Bernhard Richter <bernhard.richter@gmail.com>
2014-09-16 13:53:25 +0200

    Deleted Annotation.cs

---
+++ iOS/IntegrationTests/Android/AndroidTests/TestsSample.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using LightInject;
+namespace AndroidTests
+{
+	[TestFixture]
+	public class TestsSample
+	{
+		
+		[SetUp]
+		public void Setup ()
+		{
+			ServiceContainer container = new ServiceContainer();
+			container.GetInstance2<string> ();
+		}
+
+		
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void Pass ()
+		{
+			Console.WriteLine ("test1");
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+
+		[Test]
+		public void Inconclusive ()
+		{
+			Assert.Inconclusive ("Inconclusive");
+		}
+	}
+}
+
