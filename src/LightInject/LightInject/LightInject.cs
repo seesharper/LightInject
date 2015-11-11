0d12451a73c836f9a41bc87ca5981cf05ecfefb0
bernhard.richter@gmail.com <bernhard.richter@gmail.com>
2015-11-11 11:08:21 +0100

    #224 removed dead code

--- src/LightInject/LightInject/LightInject.cs
+++ src/LightInject/LightInject/LightInject.cs
@@ -1033,41 +1033,7 @@ namespace LightInject
         /// <returns>The <see cref="ScopeManager"/> that is responsible for managing scopes.</returns>
         ScopeManager GetScopeManager();
     }
-
-    /// <summary>
-    /// Extends the <see cref="Expression"/> class.
-    /// </summary>
-    public static class ExpressionExtensions
-    {
-        /// <summary>
-        /// Flattens the <paramref name="expression"/> into an <see cref="IEnumerable{T}"/>.
-        /// </summary>
-        /// <param name="expression">The target <see cref="Expression"/>.</param>
-        /// <returns>The <see cref="Expression"/> represented as a list of sub expressions.</returns>
-        public static IEnumerable<Expression> AsEnumerable(this Expression expression)
-        {
-            var flattener = new ExpressionTreeFlattener();
-            return flattener.Flatten(expression);
-        }
-
-        private class ExpressionTreeFlattener : ExpressionVisitor
-        {
-            private readonly ICollection<Expression> nodes = new Collection<Expression>();
-
-            public IEnumerable<Expression> Flatten(Expression expression)
-            {
-                Visit(expression);
-                return nodes;
-            }
-
-            public override Expression Visit(Expression node)
-            {
-                nodes.Add(node);
-                return base.Visit(node);
-            }
-        }               
-    }
-
+   
     /// <summary>
     /// Contains a set of helper method related to validating
     /// user input.
