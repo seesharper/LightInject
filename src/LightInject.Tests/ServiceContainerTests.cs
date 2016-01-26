7e3e188a6af81caf22dccfd2073b92a40b62ad34
Bernhard Richter <bernhard.richter@gmail.com>
2016-01-26 20:55:14 +0100

    enabled verification tests

--- src/LightInject.Tests/ServiceContainerTests.cs
+++ src/LightInject.Tests/ServiceContainerTests.cs
@@ -1120,7 +1120,7 @@ namespace LightInject.Tests
             }
         }
  
-        //[Fact]
+        [Fact]
         public void GetInstance_SingletonUsingMultipleThreads_ReturnsSameInstance()
         {
             var container = CreateContainer();
