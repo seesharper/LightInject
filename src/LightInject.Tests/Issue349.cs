c44012d1eb7b418ba27213dbb70e23d36d4e8244
Bernhard Richter <bernhard.richter@gmail.com>
2017-09-14 09:31:46 +0200

    fixes #349

---
+++ src/LightInject.Tests/Issue349.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace LightInject.Tests
+{
+    public class Issue349
+    {
+        [Fact]
+        public void ShouldHandleDisposeWhenServiceFactoryIsRegistered()
+        {
+            var container = new ServiceContainer();
+            container.RegisterInstance<IServiceFactory>(container);
+            container.GetInstance<IServiceFactory>();
+            container.Dispose();
+        }
+
+        [Fact]
+        public void ShouldHandleDisposeWhenServiceContainerIsRegistered()
+        {
+            var container = new ServiceContainer();
+            container.RegisterInstance<IServiceContainer>(container);
+            container.GetInstance<IServiceContainer>();
+            container.Dispose();
+        }
+    }
+}
