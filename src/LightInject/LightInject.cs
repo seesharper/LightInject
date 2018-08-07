65a9d4629b869674157e99181f3ad4e80c5092d6
Bernhard Richter <bernhard.richter@gmail.com>
2018-08-07 12:07:05 +0200

    Bumped version to 5.1.6

--- src/LightInject/LightInject.cs
+++ src/LightInject/LightInject.cs
@@ -21,7 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 ******************************************************************************
-    LightInject version 5.1.4
+    LightInject version 5.1.6
     http://www.lightinject.net/
     http://twitter.com/bernhardrichter
 ******************************************************************************/
