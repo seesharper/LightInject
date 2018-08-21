f1af7f15f7ee9ef8451d8de953b23b5e6ca4772a
Bernhard Richter <bernhard.richter@gmail.com>
2018-08-21 10:54:30 +0200

    Fixed version number in source file

--- src/LightInject/LightInject.cs
+++ src/LightInject/LightInject.cs
@@ -21,7 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 ******************************************************************************
-    LightInject version 5.1.6
+    LightInject version 5.1.7
     http://www.lightinject.net/
     http://twitter.com/bernhardrichter
 ******************************************************************************/
