0534f416b340b2fd1b7d11914c3d0064fbe18293
Håvard Moås <haavamoa@gmail.com>
2023-01-01 00:56:31 +0100

    Update LightInject.cs

--- src/LightInject/LightInject.cs
+++ src/LightInject/LightInject.cs
@@ -11,7 +11,7 @@
 /*********************************************************************************
     The MIT License (MIT)
 
-    Copyright (c) 2022 bernhard.richter@gmail.com
+    Copyright (c) 2023 bernhard.richter@gmail.com
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
